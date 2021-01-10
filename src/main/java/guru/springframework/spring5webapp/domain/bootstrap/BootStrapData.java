6cc07a2988dd82241fa9a7a6fda1f3758476bb84
Michael Morrison <morri909@umn.edu>
2021-01-10 11:57:32 -0600

    Add publisher POJO and repository.  Add publisher creation to bootstrap

--- src/main/java/guru/springframework/spring5webapp/domain/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/domain/bootstrap/BootStrapData.java
@@ -2,20 +2,26 @@ package guru.springframework.spring5webapp.domain.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.domain.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.domain.repositories.BookRepository;
+import guru.springframework.spring5webapp.domain.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
 
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
@@ -38,5 +44,13 @@ public class BootStrapData implements CommandLineRunner {
 
 		System.out.println("Started in Bootstrap");
 		System.out.println("Number of Books: " + bookRepository.count());
+
+		Publisher publisher = new Publisher("Test", "100 test ave", "Minneapolis", "MN", "55419");
+
+		publisherRepository.save(publisher);
+		Optional<Publisher> result = publisherRepository.findById(publisher.getId());
+
+		System.out.println("Number of Publishers: " + publisherRepository.count());
+		System.out.println(result.get());
 	}
 }
